d3853a838f744b46290b69961ec17757666eebdd
sascha kovač <143137505+saschakovac@users.noreply.github.com>
2024-05-21 13:26:49 -0400

    Update Main.java
    Added Stage 2/5

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package asciimirror;
 
 public class Main {
+    // Stage 1/5
     public static void main(String[] args) {
         System.out.println("                    _______ \n");
         System.out.println("                   < hello >");
@@ -11,4 +12,16 @@ public class Main {
         System.out.println("   | w----||                \n");
         System.out.println("   ||     ||                \n");
     }
+
+
+    // Stage 2/5
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Input the file path:");
+        String path = sc.nextLine();
+        System.out.println(path + " Moo :)");
+        System.out.println("            ^__^\n");
+        System.out.println("    _______/(oo)\n");
+        System.out.println("/\\/(       /(__)\n");
+        System.out.println("   | w----||    \n");
+        System.out.println("   ||     ||    \n");
 }
