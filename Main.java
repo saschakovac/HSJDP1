6392b40b19e6f18f8820534e6b4b2a1329fce8a1
sascha kovač <143137505+saschakovac@users.noreply.github.com>
2024-05-21 13:15:22 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package asciimirror;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("                    _______ \n");
+        System.out.println("                   < hello >");
+        System.out.println("                    ------- \n");
+        System.out.println("            ^__^   /        \n");
+        System.out.println("    _______/(oo)  /         \n");
+        System.out.println("/\\/(       /(__)            \n");
+        System.out.println("   | w----||                \n");
+        System.out.println("   ||     ||                \n");
+    }
+}
